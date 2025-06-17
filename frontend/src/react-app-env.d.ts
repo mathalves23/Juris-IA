639d434fc808fd967f4b367be5402e0a7a5cd4b3
Matheus De Araujo Alves <matheus.dearaujo@mercadolivre.com>
2025-06-17 10:46:35 -0300

    fix: Corrige tipos do React e remove declaracoes customizadas conflitantes

--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,12 +1,1 @@
-/// <reference types="react-scripts" />
-
-// Tipos básicos necessários
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.gif';
-declare module '*.svg';
-declare module '*.css';
-declare module '*.scss';
-declare module '*.module.css';
-declare module '*.module.scss'; 
+/// <reference types="react-scripts" /> 
